b1e8411e2f4e432f47c62f9caf8ca49100bd3a4b
risrael-1 <rapha.israel@live.fr>
2020-03-28 20:36:07 +0100

    création de la methode get dans server.js + collection de user

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
+const fs = require('fs')
+const util = require('util')
 const express = require('express')
-const app = express()
+const MongoClient = require('mongodb').MongoClient;
+
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const DATABASE_NAME = 'notes-api';
+const COLLECTION_NAME = 'user';
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/user', function (req, res) {
+  const user = req.query.user
+  if (user) {
+    res.send('Bonjour, ' + user + ' !')
+  } else {
+    res.send('User : ')
+    res.send('Password : ')
+  }
 })
+
+;(async () => {
+  console.log(`Connecting to ${DATABASE_NAME}...`)
+  const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  await client.connect()
+  collection = client.db(DATABASE_NAME).collection(COLLECTION_NAME)
+  console.log(`Successfully connected to ${DATABASE_NAME}`)
+  app.listen(PORT, function () {
+    console.log('Example app listening on port ' + PORT)
+  })
+  // await client.close() // should be done when the server is going down
+})()
